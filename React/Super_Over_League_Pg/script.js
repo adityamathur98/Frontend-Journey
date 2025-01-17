2e0c66662141577cb32dca48bc08afa477d872fb
adityamathur98 <adityamathur1889@gmail.com>
2025-01-17 22:06:22 +0530

    Prepare Dynamic  Responsive Web Applications using React

---
+++ React/Super_Over_League_Pg/script.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="super-over-league-container">
+    <h1 className="super-over-league-heading">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="rcb-img"
+        className="logo-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="csk-img"
+        className="logo-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
